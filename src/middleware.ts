61d43a5d6f5edd904dd1c2a54e438d8e8197fffd
joao1barbosa <joao1barbosa@hotmail.com>
2026-01-02 21:39:01 -0300

    chore: add type to supa client on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { createServerClient } from '@supabase/ssr';
+import { Database } from '@/types/supabase';
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
@@ -8,7 +9,7 @@ export async function middleware(request: NextRequest) {
         },
     });
 
-    const supabase = createServerClient(
+    const supabase = createServerClient<Database>(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
